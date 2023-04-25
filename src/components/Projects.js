7b667b1b10cc6adc9b0a56c018171b99b9199467
Brodie Kime <brodie.kime@gmail.com>
2023-04-24 21:14:08 -0400

    added projects pictures and names

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,15 +5,42 @@ function Projects() {
   const sectionRef = useRef(null);
   const isVisible = useIntersectionObserver(sectionRef);
 
-
-    return (
-      <h1       
-      className={`home-container ${isVisible ? "fade-in" : "fade-out"}`}
-      ref={sectionRef}>
-      Projects
+  return (
+    <div  className={` ${isVisible ? "fade-in" : "fade-out"}`} ref={sectionRef}>
+      <h1>
+        Projects
       </h1>
-    );
-    
-  }
-  
-  export default Projects;
+      <h2>SampleWav</h2>
+      <div>
+        <img
+          className="project-pics"
+          src={require("../images/samplewav.png")}
+          alt="SampleWav logo"
+        />
+      </div>
+      <div>
+        <h2>BestFest</h2>
+        <img
+          className="project-pics"
+          src={require("../images/bestfest.png")}
+          alt="BestFest logo"
+        />
+      </div>
+      <div>
+        <h2>Save The Rave</h2>
+        <img
+          className="project-pics"
+          src={require("../images/save-the-rave.png")}
+          alt="BestFest logo"
+        />
+        <img
+          className="project-pics"
+          src={require("../images/gameplay.png")}
+          alt="Screenshot of the gameplay of the Save The Rave game"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
