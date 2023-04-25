159851273a19f8354b7f53e83a759d9c25e8b581
Brodie Kime <brodie.kime@gmail.com>
2023-04-25 12:28:54 -0400

    working on projects still

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,64 +1,108 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import useIntersectionObserver from "./useIntersectionObserver";
 
 function Projects() {
   const sectionRef = useRef(null);
   const isVisible = useIntersectionObserver(sectionRef);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: false,
+  };
+
   return (
-    <div  id="projects-slider" className={` ${isVisible ? "fade-in" : "fade-out"}`} ref={sectionRef}>
-      <h1>
-        Projects
-      </h1>
-      <h2>SampleWav</h2>
-      <img
-          className="project-logos"
-          src={require("../images/samplewav-logo.png")}
-          alt="SampleWav logo"
-        />
-      <p>- SampleWav is website that allows users to upload, browse, and download music samples and functions as a social media site allowing profiles, followers, comments, and reposts.</p>
-        <p>- React, Javascript, Node.js, Express.js, MongoDB, Cloudinary, Tailwind CSS, CSS & HTML</p>
-      <div>
-        <img
-          className="project-pics"
-          src={require("../images/samplewav.png")}
-          alt="SampleWav landing page screenshot"
-        />
-      </div>
-      <div id="bestfest">
-        
-        <img
-          className="project-logos"
-          src={require("../images/best-fest-logo.png")}
-          alt="Best Fest logo"
-        />
-        <p>- <h2>Best Fest</h2> is website that allows users singup, create a profile, then rate, review, and comment on music festival experiences</p>
-        <p>- Node.js, Express.js, Handlebars, MongoDB, CSS & HTML</p>
-        <img
-          className="project-pics"
-          src={require("../images/best-fest.png")}
-          alt="BestFest landing page screenshot"
-        />
-      </div>
-      <div>
-        
-        <img
-          className="project-logos"
-          src={require("../images/save-the-rave-logo.png")}
-          alt="Save The Rave logo"
-        />
-        <p>- <h2>Save The Rave</h2> is an arcade-style game with music and a fun theme</p>
-        <p>- Javascript, HTML, CSS, Canvas</p>
-        <img
-          className="rave-pics"
-          src={require("../images/save-the-rave.png")}
-          alt="Save the Rave intro screen"
-        />
-        <img
-          className="rave-pics"
-          src={require("../images/gameplay.png")}
-          alt="Screenshot of the gameplay of the Save The Rave game"
-        />
+    <div
+      id="projects-slider"
+      className={` ${isVisible ? "fade-in" : "fade-out"}`}
+      ref={sectionRef}
+    >
+      <h1>Projects</h1>
+      <div className="slider-container">
+      <Slider {...settings}>
+        <div id="samplewav" className="center-slides">
+          <img
+            className="samplewav-logo"
+            src={require("../images/samplewav-logo.png")}
+            alt="SampleWav logo"
+          />
+          <div className="skills">
+            <div className="skill">React</div>
+            <div className="skill">Javascript</div>
+            <div className="skill">Node.js</div>
+            <div className="skill">Express.js</div>
+            <div className="skill">MongoDB</div>
+            <div className="skill">Cloudinary</div>
+            <div className="skill">Tailwind CSS</div>
+            <div className="skill">CSS</div>
+            <div className="skill">HTML</div>
+          </div>
+          <p className="project-description">
+            <em>SampleWav</em> is a website that allows users to upload, browse,
+            and download music samples and functions as a social media site
+            allowing profiles, followers, comments, and reposts.
+          </p>
+          <img
+            className="project-pics"
+            src={require("../images/samplewav.png")}
+            alt="SampleWav landing page screenshot"
+          />
+        </div>
+        <div id="best-fest">
+          <img
+            className="best-fest-logo"
+            src={require("../images/best-fest-logo.png")}
+            alt="Best Fest logo"
+          />
+          <div className="skills">
+            <div className="skill">Node.js</div>
+            <div className="skill">Express.js</div>
+            <div className="skill">Handlebars</div>
+            <div className="skill">MongoDB</div>
+            <div className="skill">CSS</div>
+            <div className="skill">HTML</div>
+          </div>
+          <p className="project-description">
+            <em>Best Fest</em> is a website that allows users to sign up, create
+            a profile, then rate, review, and comment on music festival
+            experiences.
+          </p>
+          <img
+            className="project-pics"
+            src={require("../images/best-fest.png")}
+            alt="BestFest landing page screenshot"
+          />
+        </div>
+        <div id="save-the-rave">
+          <img
+            className="save-the-rave-logo"
+            src={require("../images/save-the-rave-logo.png")}
+            alt="Save The Rave logo"
+          />
+          <div className="skills">
+            <div className="skill">Javascript</div>
+            <div className="skill">HTML</div>
+            <div className="skill">CSS</div>
+            <div className="skill">Firebase</div>
+          </div>
+          <p className="project-description">
+            <em>Save The Rave</em> is a game that challenges players to save a
+            rave by collecting music notes while avoiding obstacles. The game
+            also features a leaderboard to track high scores.
+          </p>
+          <img
+            className="project-pics"
+            src={require("../images/save-the-rave.png")}
+            alt="Save The Rave game screenshot"
+          />
+        </div>
+      </Slider>
       </div>
     </div>
   );
