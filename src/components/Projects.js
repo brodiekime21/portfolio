26df4fcb9ca688b342de31f14f8e6237a5745f3b
Brodie Kime <brodie.kime@gmail.com>
2023-04-24 21:22:40 -0400

    added projects info and changed pic sizes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,8 @@ function Projects() {
         Projects
       </h1>
       <h2>SampleWav</h2>
+      <p>- A website that allows users to upload, browse, and download music samples and functions as a social media site allowing profiles, followers, comments, and reposts.</p>
+        <p>- React, Javascript, Node.js, Express.js, MongoDB, Cloudinary, Tailwind CSS, CSS & HTML</p>
       <div>
         <img
           className="project-pics"
@@ -20,6 +22,8 @@ function Projects() {
       </div>
       <div>
         <h2>BestFest</h2>
+        <p>- A website that allows users to rate, review, and comment on festival experiences</p>
+        <p>- Node.js, Express.js, Handlebars, MongoDB, CSS & HTML</p>
         <img
           className="project-pics"
           src={require("../images/bestfest.png")}
@@ -28,13 +32,15 @@ function Projects() {
       </div>
       <div>
         <h2>Save The Rave</h2>
+        <p>- An arcade-style game with music and a fun theme</p>
+        <p>- Javascript, HTML, CSS, Canvas</p>
         <img
-          className="project-pics"
+          className="rave-pics"
           src={require("../images/save-the-rave.png")}
           alt="BestFest logo"
         />
         <img
-          className="project-pics"
+          className="rave-pics"
           src={require("../images/gameplay.png")}
           alt="Screenshot of the gameplay of the Save The Rave game"
         />
