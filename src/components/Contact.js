e7955a3fc86c993cb793b642d40f2535c5300017
Brodie Kime <brodie.kime@gmail.com>
2023-04-24 20:03:26 -0400

    made more mobile responsive and adjusted the main welcome image message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,18 +8,18 @@ function Contact() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div       
-      className={`home-container boxes flex flex-col items-center ${isVisible ? "fade-in" : "fade-out"}`}
+      className={`contact-container ${isVisible ? "fade-in" : "fade-out"}`}
       ref={sectionRef}>
-        <h1 className="text-2xl font-bold mb-4 text-white">
-          Please feel free to reach out to my email below or connect with me on         
-        </h1>
+        <h1 className="">
+          Reach out to my email below or connect with me on         
           <a 
           href="https://www.linkedin.com/in/brodie-kime/"
-          className="mr-6 text-4xl font-bold hover:text-white focus:outline-none focus:ring focus:ring-white"
+          className=""
         >
           LinkedIn
         </a>
         <a className="text-4xl font-bold mb-4 text-white" href="mailto:brodie.kime@gmail.com">Email me @ brodie.kime@gmail.com</a>
+        </h1>
       </div>
     </div>
   );
