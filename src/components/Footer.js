f4e1baa19733bef03220a10c0d05b4c519c9f4f1
Brodie Kime <brodie.kime@gmail.com>
2023-05-05 13:26:25 -0400

    more media queries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,7 @@
 function Footer() {
   return (
     <div id="footer">
-      <div id="footer-links">
-        <a
-          href="https://www.linkedin.com/in/brodie-kime/"
-          activeClassName="text-white"
-          className="personal-links footer-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="icons"
-            src={require("../images/linkedin-icon.png")}
-            alt="LinkedIn icon"
-          />
-          LinkedIn
-        </a>
-        <a
-          href="https://github.com/brodiekime21"
-          activeClassName="text-white"
-          className="personal-links footer-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="icons"
-            src={require("../images/github-icon.png")}
-            alt="GitHub icon"
-          />
-          GitHub
-        </a>
-        <a
-          href="https://github.com/brodiekime21"
-          activeClassName="text-white"
-          className="personal-links footer-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="icons"
-            src={require("../images/email-icon.png")}
-            alt="GitHub icon"
-          />
-          Email
-        </a>
-      </div>
-      <h2>&copy; 2023, Built and designed by Brodie Kime</h2>
+      <h2> Copyright &copy; 2023 - Brodie Kime</h2>
     </div>
   );
 }
