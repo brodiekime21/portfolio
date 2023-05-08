6c722efd1902ef910335ae0ba677e737b0f3bf04
Brodie Kime <brodie.kime@gmail.com>
2023-05-08 09:06:04 -0400

    established main screen and got rid of changing logo sizes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 function Contact() {
   return (
     <div id="contact">
-      <p className="contact">Currently looking for new opportunities.<br></br>Click below to reach out.
+      <p className="contact">Currently looking for new opportunities.<br></br>Click to reach out.
       </p>
       <h1 className="contact">
         <a className=" contact-button" href="mailto:brodie.kime@gmail.com">brodie.kime@gmail.com</a>
